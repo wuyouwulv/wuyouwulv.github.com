fb308c07ef9674a5f8264fc3120d4af16a64d985
wuyouwulv <iwuyouwulv@yahoo.com>
2015-04-12 19:41:39 +0800

    hello

--- js/playmusic.js
+++ js/playmusic.js
@@ -1,14 +1,10 @@
 var audio = document.createElement("audio");
 	
 function choose_music() {
-	var id = Math.floor(Math.random()*6);
+	var id = Math.floor(Math.random()*2);
 	var music_arr = new Array(2);
-	music_arr[0] = "music/匆匆那年 - 王菲.mp3";
-	music_arr[1] = "music/天堂 - 唐朝乐队.mp3";
-	music_arr[2] = "music/寂寞寂寞就好 - 田馥甄.mp3";
-	music_arr[3] = "music/魔鬼中的天使 - 田馥甄.mp3";
-	music_arr[4] = "music/信仰之名 - TFBOYS.mp3";
-	music_arr[5] = "music/雨樱花 - 飞儿乐团.mp3";
+	music_arr[0] = "music/慢慢来比较快 - 梁静茹.mp3";
+	music_arr[1] = "music/七里香 - 周杰伦.mp3";
 	// music_arr[] = "music/";
 	return music_arr[id];
 }
@@ -25,5 +21,10 @@ function play_music() {
 		audio.play();
 	}
 }
+
+function stop_music() {
+	audio.src = "";
+	audio.play();
+}
 			
-play_music();
+//play_music();
